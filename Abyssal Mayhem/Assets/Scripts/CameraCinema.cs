a4a03ff610d65942544cbb89a278a795bea4962d
Arif-Khalid <arifkhalid99@gmail.com>
2022-05-29 16:55:21 +0800

    Added components for cinematography in map1 scene Tweaked lighting for optimisation in infinite spawn scene

---
+++ Abyssal Mayhem/Assets/Scripts/CameraCinema.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCinema : MonoBehaviour
+{
+    Animator animator;
+    private bool rotating;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (rotating)
+            {
+                animator.SetBool("Rotate", false);
+                rotating = false;
+            }
+            else
+            {
+                animator.SetBool("Rotate", true);
+                rotating = true;
+            }   
+        }
+    }
+}
